7fb1f9e12c1df06a52eb9e541023471af94375ff
NLSS | Bilgehan <a.bilgehan.serce@gmail.com>
2025-05-31 20:15:51 +0300

    5.31.25 | Scripts for Ch01 of Design Patterns

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,35 @@
 #include <iostream>
-#include <string>
 
-enum MealType {
-	DEFAULT,
-	VEGETERIAN,
-	VEGAN,
-	LOW_FAT
+// Mixin: Printable
+class Printable {
+public:
+    void print() const {
+        std::cout << "Printed..." << std::endl;
+        return;
+    }
 };
 
-struct Passenger
-{
-	std::string name;
-	bool isPassennger;
-	MealType meal;
-	long ID;
+// Mixin: Savable
+class Savable {
+public:
+    void save() const {
+        std::cout << "Saved..." << std::endl;
+        return;
+    }
+};
 
+// Class: Document
+//   + Printable (Mixin)
+//   + Saveble (Mixin)
+class Document : public Printable, public Savable {
+public:
+    // Document-specific methods here
 };
 
 int main() {
-
-	Passenger *person;
-
-	person = new Passenger;
-
-	person->name = "john Doe";
-	person->isPassennger = true;
-	person->meal = VEGETERIAN;
-	person->ID = 1008937867;
-	
-	if (person->isPassennger) {
-		std::cout << "Hello, " << person->name << std::endl;
-		std::cout << "ID: " << person->ID << std::endl;
-		std::cout << "Meal Preference: " << person->meal << std::endl;
-		return 0;
-	}	
-	std::cout << "NOT a passenger!" << std::endl;
-	
-	delete person;
-
-	return -1;
+    Document doc;
+    doc.print();  // From Printable mixin
+    doc.save();   // From Savable mixin
+    
+	return 0;
 }
